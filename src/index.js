7c2c5866909e1d184d2b5aa6482c7454f7e025e1
Myka Enobs <mykaenobs@gmail.com>
2021-03-18 23:30:14 +0100

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello world! Don't Starting with heroku why na</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello world! Ok continue Starting with heroku why na</h1>, document.getElementById("root"))
