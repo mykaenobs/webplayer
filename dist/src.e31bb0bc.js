2ff2c74618ce90b6c5bae5b0be04c7f8ed944548
Myka Enobs <mykaenobs@gmail.com>
2021-03-19 23:05:29 +0100

    test

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -29971,7 +29971,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51763" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51822" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
